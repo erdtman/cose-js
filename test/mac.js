4c5bb28698db287eceb5764cff348511402e020f
Joe Hildebrand <joe-github@cursive.net>
2016-08-24 09:58:23 -0600

    Move to ava for testing

---
+++ test/mac.js
@@ -0,0 +1,31 @@
+const cbor = require('cbor');
+const cose = require('../');
+const test = require('ava');
+
+var hProtected = {
+  "alg": "SHA-256",
+  "content_type": 5,
+};
+var key = "secret"
+var payload = {"hello": "world"};
+var hUnprotected = {};
+
+test('basic mac', t => {
+  return cose.COSE_Mac.create(hProtected,
+                              hUnprotected,
+                              cbor.encode(payload),
+                              key)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    return cose.COSE_Mac.read(buf, key);
+  })
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    return cbor.decodeFirst(buf);
+  })
+  .then((obj) => {
+    t.deepEqual(obj, payload);
+  });
+});
