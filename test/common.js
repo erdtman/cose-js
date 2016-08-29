4adab975ce9f762521d03a098aef1dec216ac1db
Joe Hildebrand <joe-github@cursive.net>
2016-08-29 16:07:31 -0600

    Add a little coverage.  Add `npm run dev` which (after an `npm install`) will do live reloads of the coverage output as you modify the tests

---
+++ test/common.js
@@ -0,0 +1,21 @@
+/* jshint esversion: 6 */
+/* jslint node: true */
+'use strict';
+const test = require('ava');
+const cose = require('../');
+
+test('translate headers', (t) => {
+  let h = cose.common.TranslateHeaders({});
+  t.is(h.constructor.name, 'Map');
+  h = cose.common.TranslateHeaders({alg: 'SHA-256', crit: 2});
+  t.is(h.constructor.name, 'Map');
+  t.is(h.size, 2);
+  t.is(h.get(cose.common.HeaderParameters.alg), 'SHA-256');
+  t.is(h.get(cose.common.HeaderParameters.crit), 2);
+});
+
+test('invalid', (t) => {
+  t.throws(() => {
+    cose.common.TranslateHeaders({'fizzle stomp': 12});
+  });
+});
