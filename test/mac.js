5176c6ad4e90896b08f2d1507abb1019ff3fcde5
Samuel Erdtman <erdtman@spotify.com>
2017-03-14 07:01:47 +0100

    uppdated and tested mac with example vectors

--- test/mac.js
+++ test/mac.js
@@ -1,32 +1,46 @@
 const cbor = require('cbor');
 const cose = require('../');
 const test = require('ava');
+const utils = require('../test-helpers/failures.js');
+const jsonfile = require('jsonfile');
+const base64url = require('base64url');
+const files = ['Examples/mac0-tests/mac-pass-01.json',
+  'Examples/mac0-tests/mac-pass-02.json',
+  'Examples/mac0-tests/mac-pass-03.json'];
 
-var hProtected = {
-  'alg': 'SHA-256',
-  'content_type': 5
-};
-var key = 'secret';
-var payload = {'hello': 'world'};
-var hUnprotected = {};
+files.forEach(function (file) {
+  const example = jsonfile.readFileSync(file);
+  let external = example.input.mac0.external;
+  external = external ? new Buffer(external, 'hex') : undefined;
 
-test('basic mac', t => {
-  return cose.mac.create(hProtected,
-                         hUnprotected,
-                         cbor.encode(payload),
-                         key)
-  .then((buf) => {
-    t.true(Buffer.isBuffer(buf));
-    t.true(buf.length > 0);
-    return cose.mac.read(buf, key);
-  })
-  .then((buf) => {
-    t.true(Buffer.isBuffer(buf));
-    t.true(buf.length > 0);
-    return cbor.decodeFirst(buf);
-  })
-  .then((obj) => {
-    t.deepEqual(obj, payload);
+  test('CREATE: ' + example.title, t => {
+    return cose.mac.create({p: example.input.mac0.protected,
+      u: example.input.mac0.unprotected},
+      Buffer.from(example.input.plaintext),
+      [{'key': base64url.toBuffer(example.input.mac0.recipients[0].key.k)}],
+      external)
+    .then((buf) => {
+      t.true(Buffer.isBuffer(buf));
+      t.true(buf.length > 0);
+      for (let failure in example.input.failures) {
+        if (!utils[failure]) {
+          continue;
+        }
+        buf = utils[failure](buf, example.input.failures[failure]);
+      }
+      t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
+    });
+  });
+
+  test('READ: ' + example.title, t => {
+    return cose.mac.read(example.output.cbor,
+      base64url.toBuffer(example.input.mac0.recipients[0].key.k),
+      external)
+    .then((buf) => {
+      t.true(Buffer.isBuffer(buf));
+      t.true(buf.length > 0);
+      t.is(buf.toString('utf8'), example.input.plaintext);
+    });
   });
 });
 
@@ -35,5 +49,5 @@ test('errors', t => {
     cose.mac.create({});
   });
   t.throws(() => cose.mac.create({'alg': 'fizzle blorp'}));
-  t.throws(cose.mac.read(cbor.encode('foo'), key));
+  t.throws(cose.mac.read(cbor.encode('foo'), 'key'));
 });
