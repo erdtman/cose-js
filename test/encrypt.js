df0230ca21a96c68fb05ceee06341f53a70352a2
Samuel Erdtman <erdtman@spotify.com>
2017-08-28 22:14:53 +0200

    changed to real tag value

--- test/encrypt.js
+++ test/encrypt.js
@@ -1,93 +1,206 @@
-// const cbor = require('cbor');
+/* jshint esversion: 6 */
+/* jslint node: true */
+'use strict';
+
 const cose = require('../');
 const test = require('ava');
-const fs = require('fs');
-// const utils = require('../test-helpers/failures.js');
 const jsonfile = require('jsonfile');
 const base64url = require('base64url');
-let files;
-if (fs.existsSync('Examples')) {
-  files = ['Examples/aes-gcm-examples/aes-gcm-01.json',
-    'Examples/aes-gcm-examples/aes-gcm-02.json',
-    'Examples/aes-gcm-examples/aes-gcm-03.json'];
-} else {
-  files = ['test/Examples/aes-gcm-examples/aes-gcm-01.json',
-    'test/Examples/aes-gcm-examples/aes-gcm-02.json',
-    'test/Examples/aes-gcm-examples/aes-gcm-03.json',
-    'test/Examples/aes-gcm-examples/aes-gcm-05.json'];
-}
 
 function randomSource (bytes) {
   if (bytes === 12) {
-    return new Buffer('02D1F7E6F26C43D4868D87CE', 'hex');
+    return Buffer.from('02D1F7E6F26C43D4868D87CE', 'hex');
   } else {
-    return new Buffer('61A7', 'hex');
+    return Buffer.from('61A7', 'hex');
   }
 }
 
-files.forEach(function (file) {
-  const example = jsonfile.readFileSync(file);
+test('create aes-gcm-01', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-01.json');
+  const p = example.input.enveloped.protected;
+  const u = example.input.enveloped.unprotected;
+  const plaintext = Buffer.from(example.input.plaintext);
 
-  test('CREATE: ' + example.title, t => {
-    const recipients = [{
-      'key': base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
-      'u': example.input.enveloped.recipients[0].unprotected
-    }];
+  const recipients = [{
+    'key': base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
+    'u': example.input.enveloped.recipients[0].unprotected
+  }];
 
-    if (example.input.enveloped.unprotected && example.input.enveloped.unprotected.partialIV_hex) {
-      example.input.enveloped.unprotected.Partial_IV = Buffer.from(example.input.enveloped.unprotected.partialIV_hex, 'hex');
-      delete example.input.enveloped.unprotected.partialIV_hex;
-    }
+  const options = {
+    'randomSource': randomSource
+  };
 
-    let contextIv;
-    if (example.input.enveloped.unsent && example.input.enveloped.unsent.IV_hex) {
-      contextIv = Buffer.from(example.input.enveloped.unsent.IV_hex, 'hex');
-      contextIv[10] = 0;
-      contextIv[11] = 0;
-    }
-
-    const options = {
-      'randomSource': randomSource,
-      'externalAAD': undefined,
-      'contextIv': contextIv
-    };
-    return cose.encrypt.create({p: example.input.enveloped.protected,
-      u: example.input.enveloped.unprotected},
-      Buffer.from(example.input.plaintext),
-      recipients,
-      options)
-    .then((buf) => {
-      t.true(Buffer.isBuffer(buf));
-      t.true(buf.length > 0);
-      t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
-    });
-  });
-
-  test('READ: ' + example.title, t => {
-    let contextIv;
-    if (example.input.enveloped.unsent && example.input.enveloped.unsent.IV_hex) {
-      contextIv = Buffer.from(example.input.enveloped.unsent.IV_hex, 'hex');
-      contextIv[10] = 0;
-      contextIv[11] = 0;
-    }
-
-    return cose.encrypt.read(example.output.cbor,
-      base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
-      {'contextIv': contextIv})
-    .then((buf) => {
-      t.true(Buffer.isBuffer(buf));
-      t.true(buf.length > 0);
-      t.is(buf.toString('utf8'), example.input.plaintext);
-    });
+  return cose.encrypt.create(
+    {p: p, u: u},
+    plaintext,
+    recipients,
+    options)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
   });
 });
 
-const failExample = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-04.json');
-test('FAIL: ' + failExample.title, t => {
-  cose.encrypt.read(failExample.output.cbor,
-    base64url.toBuffer(failExample.input.enveloped.recipients[0].key.k)).then((buf) => {
-      t.true(false);
-    }).catch((error) => {
-      t.is(error.message, 'Unsupported state or unable to authenticate data');
-    });
+test('create aes-gcm-02', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-02.json');
+  const p = example.input.enveloped.protected;
+  const u = example.input.enveloped.unprotected;
+  const plaintext = Buffer.from(example.input.plaintext);
+
+  const recipients = [{
+    'key': base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
+    'u': example.input.enveloped.recipients[0].unprotected
+  }];
+
+  const options = {
+    'randomSource': randomSource
+  };
+
+  return cose.encrypt.create(
+    {p: p, u: u},
+    plaintext,
+    recipients,
+    options)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
+  });
+});
+
+test('create aes-gcm-03', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-03.json');
+  const p = example.input.enveloped.protected;
+  const u = example.input.enveloped.unprotected;
+  const plaintext = Buffer.from(example.input.plaintext);
+
+  const recipients = [{
+    'key': base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
+    'u': example.input.enveloped.recipients[0].unprotected
+  }];
+
+  const options = {
+    'randomSource': randomSource
+  };
+
+  return cose.encrypt.create(
+    {p: p, u: u},
+    plaintext,
+    recipients,
+    options)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
+  });
+});
+
+test('create aes-gcm-05', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-05.json');
+  const p = example.input.enveloped.protected;
+  const u = example.input.enveloped.unprotected;
+  const plaintext = Buffer.from(example.input.plaintext);
+
+  const recipients = [{
+    'key': base64url.toBuffer(example.input.enveloped.recipients[0].key.k),
+    'u': example.input.enveloped.recipients[0].unprotected
+  }];
+
+  example.input.enveloped.unprotected.Partial_IV = Buffer.from(example.input.enveloped.unprotected.partialIV_hex, 'hex');
+  delete example.input.enveloped.unprotected.partialIV_hex;
+
+  const contextIv = Buffer.from(example.input.enveloped.unsent.IV_hex, 'hex');
+  contextIv[10] = 0;
+  contextIv[11] = 0;
+
+  const options = {
+    'randomSource': randomSource,
+    'contextIv': contextIv
+  };
+
+  return cose.encrypt.create(
+    {'p': p, 'u': u},
+    plaintext,
+    recipients,
+    options)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
+  });
+});
+
+test('decrypt aes-gcm-01', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-01.json');
+  const plaintext = example.input.plaintext;
+  const key = base64url.toBuffer(example.input.enveloped.recipients[0].key.k);
+
+  return cose.encrypt.read(example.output.cbor,
+    key)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('utf8'), plaintext);
+  });
+});
+
+test('decrypt aes-gcm-02', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-02.json');
+  const plaintext = example.input.plaintext;
+  const key = base64url.toBuffer(example.input.enveloped.recipients[0].key.k);
+
+  return cose.encrypt.read(example.output.cbor,
+    key)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('utf8'), plaintext);
+  });
+});
+
+test('decrypt aes-gcm-03', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-03.json');
+  const plaintext = example.input.plaintext;
+  const key = base64url.toBuffer(example.input.enveloped.recipients[0].key.k);
+
+  return cose.encrypt.read(example.output.cbor,
+    key)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('utf8'), plaintext);
+  });
+});
+
+test('decrypt aes-gcm-05', t => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-05.json');
+  const plaintext = example.input.plaintext;
+  const key = base64url.toBuffer(example.input.enveloped.recipients[0].key.k);
+
+  const contextIv = Buffer.from(example.input.enveloped.unsent.IV_hex, 'hex');
+  contextIv[10] = 0;
+  contextIv[11] = 0;
+
+  return cose.encrypt.read(example.output.cbor,
+    key,
+    {'contextIv': contextIv})
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('utf8'), plaintext);
+  });
+});
+
+test('decrypt aes-gcm-04', te => {
+  const example = jsonfile.readFileSync('test/Examples/aes-gcm-examples/aes-gcm-04.json');
+  const key = base64url.toBuffer(example.input.enveloped.recipients[0].key.k);
+
+  return cose.encrypt.read(example.output.cbor,
+    key)
+  .then((buf) => {
+    te.true(false);
+  }).catch((error) => {
+    te.is(error.message, 'Unsupported state or unable to authenticate data');
+  });
 });
