269c53051a3abf33565e8c83a1dff733913ad654
Samuel Erdtman <erdtman@spotify.com>
2017-10-10 23:47:20 +0200

    added mor sign tests

--- test/sign.js
+++ test/sign.js
@@ -160,3 +160,141 @@ test('verify sign-pass-03', (t) => {
     t.is(buf.toString('utf8'), example.input.plaintext);
   });
 });
+
+test('verify sign-fail-01', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-01.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Unexpected cbor tag, \'998\'');
+    });
+});
+
+test('verify sign-fail-02', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-02.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Signature missmatch');
+    });
+});
+
+test('verify sign-fail-03', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-03.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Unknown algorithm, -999');
+    });
+});
+
+test('verify sign-fail-04', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-04.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Unknown algorithm, unknown');
+    });
+});
+
+test('verify sign-fail-06', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-06.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Signature missmatch');
+    });
+});
+
+test('verify sign-fail-07', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-07.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+    .then((buf) => {
+      t.true(false);
+    }).catch((error) => {
+      t.is(error.message, 'Signature missmatch');
+    });
+});
