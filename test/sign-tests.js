5f531bb069d3b3547a37c6ea0eea49f061b1b4c1
Samuel Erdtman <erdtman@spotify.com>
2017-10-12 11:22:10 +0200

    added the last sign-test tests

--- test/sign-tests.js
+++ test/sign-tests.js
@@ -7,6 +7,57 @@ const test = require('ava');
 const jsonfile = require('jsonfile');
 const base64url = require('base64url');
 
+// TODO create ecdsa-01
+
+test('create ecdsa-01', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/ecdsa-01.json');
+  const p = example.input.sign.protected;
+  const u = example.input.sign.unprotected;
+  const plaintext = Buffer.from(example.input.plaintext);
+
+  const signers = [{
+    'key': {
+      'd': base64url.toBuffer(example.input.sign.signers[0].key.d)
+    },
+    'u': example.input.sign.signers[0].unprotected,
+    'p': example.input.sign.signers[0].protected
+  }];
+
+  return cose.sign.create(
+    {'p': p, 'u': u},
+    plaintext,
+    signers
+  )
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('hex'), example.output.cbor.toLowerCase());
+  });
+});
+
+test('verify ecdsa-01', (t) => {
+  const example = jsonfile.readFileSync('test/Examples/sign-tests/ecdsa-01.json');
+
+  const verifier = {
+    'key': {
+      'x': base64url.toBuffer(example.input.sign.signers[0].key.x),
+      'y': base64url.toBuffer(example.input.sign.signers[0].key.y),
+      'kid': example.input.sign.signers[0].key.kid
+    }
+  };
+
+  const signature = Buffer.from(example.output.cbor, 'hex');
+
+  return cose.sign.verify(
+    signature,
+    verifier)
+  .then((buf) => {
+    t.true(Buffer.isBuffer(buf));
+    t.true(buf.length > 0);
+    t.is(buf.toString('utf8'), example.input.plaintext);
+  });
+});
+
 test('create sign-pass-01', (t) => {
   const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-pass-01.json');
   const p = example.input.sign.protected;
