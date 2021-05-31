4b8c5efd12bed6b367faeae0ebd04ed7533ce1a1
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-05-31 23:24:54 +0200

    simplify tests

--- test/sign-tests.ts
+++ test/sign-tests.ts
@@ -2,138 +2,33 @@ import * as cose from '../lib/index';
 import test from 'ava';
 import { readSigningTestData } from './util'
 
-test('create ecdsa-01', async (t) => {
-  const { verifier, plaintext, headers, signers } = await readSigningTestData('test/Examples/sign-tests/ecdsa-01.json');
-  const buf = await cose.sign.create(headers, plaintext, signers);
-  const decoded = await cose.sign.verify(buf, verifier);
-  t.deepEqual(decoded, plaintext);
-});
-
-test('verify ecdsa-01', async (t) => {
-  const { verifier, signature, plaintext } = await readSigningTestData('test/Examples/sign-tests/ecdsa-01.json');
-  const buf = await cose.sign.verify(signature, verifier);
-  t.deepEqual(buf, plaintext);
-});
-
-
-for (const i of [1, 2, 3]) {
-  test(`create sign-pass-0${i}`, async (t) => {
-    const { verifier, plaintext, headers, signers } = await readSigningTestData(`test/Examples/sign-tests/sign-pass-0${i}.json`);
+const TEST_NAMES = ['ecdsa-01', 'sign-pass-01', 'sign-pass-02', 'sign-pass-03'];
+for (const name of TEST_NAMES) {
+  test(`create ${name}`, async (t) => {
+    const { verifier, plaintext, headers, signers } = await readSigningTestData(`test/Examples/sign-tests/${name}.json`);
     const buf = await cose.sign.create(headers, plaintext, signers);
     const decoded = await cose.sign.verify(buf, verifier);
     t.deepEqual(decoded, plaintext);
   });
 
-  test(`verify sign-pass-0${i}`, async (t) => {
-    const { verifier, signature, plaintext } = await readSigningTestData(`test/Examples/sign-tests/sign-pass-0${i}.json`);
+  test(`verify ${name}`, async (t) => {
+    const { verifier, signature, plaintext } = await readSigningTestData(`test/Examples/sign-tests/${name}.json`);
     const buf = await cose.sign.verify(signature, verifier);
     t.deepEqual(buf, plaintext);
   });
 }
 
 
-test('verify sign-fail-01', async (t) => {
-  const { verifier, signature } = await readSigningTestData('test/Examples/sign-tests/sign-fail-01.json');
-  await t.throwsAsync(cose.sign.verify(signature, verifier), { message: 'Unexpected cbor tag, \'998\'' });
-});
-
-
-test('verify sign-fail-02', async (t) => {
-  const { verifier, signature } = await readSigningTestData('test/Examples/sign-tests/sign-fail-02.json');
-  await t.throwsAsync(cose.sign.verify(signature, verifier), { message: 'Signature mismatch' });
-});
-
-// TODO: ADAPT ALL TESTS
-/*
-test('verify sign-fail-03', (t) => {
-  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-03.json');
-
-  const verifier = {
-    key: {
-      x: base64url.toBuffer(example.input.sign.signers[0].key.x),
-      y: base64url.toBuffer(example.input.sign.signers[0].key.y),
-      kid: example.input.sign.signers[0].key.kid
-    }
-  };
-
-  const signature = Buffer.from(example.output.cbor, 'hex');
-
-  return cose.sign.verify(
-    signature,
-    verifier)
-    .then((buf) => {
-      t.true(false);
-    }).catch((error) => {
-      t.is(error.message, 'Unknown algorithm, -999');
-    });
-});
-
-test('verify sign-fail-04', (t) => {
-  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-04.json');
-
-  const verifier = {
-    key: {
-      x: base64url.toBuffer(example.input.sign.signers[0].key.x),
-      y: base64url.toBuffer(example.input.sign.signers[0].key.y),
-      kid: example.input.sign.signers[0].key.kid
-    }
-  };
-
-  const signature = Buffer.from(example.output.cbor, 'hex');
-
-  return cose.sign.verify(
-    signature,
-    verifier)
-    .then((buf) => {
-      t.true(false);
-    }).catch((error) => {
-      t.is(error.message, 'Unknown algorithm, unknown');
-    });
-});
-
-test('verify sign-fail-06', (t) => {
-  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-06.json');
-
-  const verifier = {
-    key: {
-      x: base64url.toBuffer(example.input.sign.signers[0].key.x),
-      y: base64url.toBuffer(example.input.sign.signers[0].key.y),
-      kid: example.input.sign.signers[0].key.kid
-    }
-  };
-
-  const signature = Buffer.from(example.output.cbor, 'hex');
-
-  return cose.sign.verify(
-    signature,
-    verifier)
-    .then((buf) => {
-      t.true(false);
-    }).catch((error) => {
-      t.is(error.message, 'Signature mismatch');
-    });
-});
-
-test('verify sign-fail-07', (t) => {
-  const example = jsonfile.readFileSync('test/Examples/sign-tests/sign-fail-07.json');
-
-  const verifier = {
-    key: {
-      x: base64url.toBuffer(example.input.sign.signers[0].key.x),
-      y: base64url.toBuffer(example.input.sign.signers[0].key.y),
-      kid: example.input.sign.signers[0].key.kid
-    }
-  };
-
-  const signature = Buffer.from(example.output.cbor, 'hex');
-
-  return cose.sign.verify(
-    signature,
-    verifier)
-    .then((buf) => {
-      t.true(false);
-    }).catch((error) => {
-      t.is(error.message, 'Signature mismatch');
-    });
-});
-*/
+[
+  { name: 'sign-fail-01', message: 'Unexpected cbor tag, \'998\'', },
+  { name: 'sign-fail-02', message: 'Signature mismatch', },
+  { name: 'sign-fail-03', message: 'Unknown algorithm, -999', },
+  { name: 'sign-fail-04', message: 'Unknown algorithm, unknown', },
+  { name: 'sign-fail-06', message: 'Signature mismatch', },
+  { name: 'sign-fail-07', message: 'Signature mismatch', },
+].forEach(({ name, message }, i) => {
+  test('verify ' + name, async (t) => {
+    const { verifier, signature } = await readSigningTestData(`test/Examples/sign-tests/${name}.json`);
+    await t.throwsAsync(cose.sign.verify(signature, verifier), { message });
+  });
+})
